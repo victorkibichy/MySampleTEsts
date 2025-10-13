4a124bb4991844ca9c68b96a9db0e8c83461f229
Kibichy <kibichy>
2025-10-13 12:09:14 +0300

    Initial Commit

---
+++ MySampleTEsts/MySampleTEstsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySampleTEstsApp.swift
+//  MySampleTEsts
+//
+//  Created by Vic on 13/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct MySampleTEstsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
